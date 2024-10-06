744b0084ab6ff76cda4cda6be04949ddd8be6b8b
1VeniVediVeci1 <87921455+1VeniVediVeci1@users.noreply.github.com>
2024-10-07 02:36:40 +0800

    Update Text.vue
    Signed-off-by: 1VeniVediVeci1 <87921455+1VeniVediVeci1@users.noreply.github.com>

--- src/views/chat/components/Message/Text.vue
+++ src/views/chat/components/Message/Text.vue
@@ -37,8 +37,7 @@ const mdi = new MarkdownIt({
 })
 const markdownItKatex = require('markdown-it-katex');
 mdi.use(mila, { attrs: { target: '_blank', rel: 'noopener' } })
-//mdi.use(mdKatex, { blockClass: 'katexmath-block rounded-md p-[10px]', errorColor: ' #cc0000' })
-mdi.use(markdownItKatex, { blockClass: 'katexmath-block rounded-md p-[10px]', errorColor: ' #cc0000' })
+mdi.use(mdKatex, { blockClass: 'katexmath-block rounded-md p-[10px]', errorColor: ' #cc0000' })
   
 const wrapClass = computed(() => {
   return [
