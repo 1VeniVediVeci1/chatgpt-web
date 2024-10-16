7b77567735cac3a57b5f017970b8ddd723c5867a
1VeniVediVeci1 <87921455+1VeniVediVeci1@users.noreply.github.com>
2024-10-16 17:28:56 +0800

    Update index.vue
    Signed-off-by: 1VeniVediVeci1 <87921455+1VeniVediVeci1@users.noreply.github.com>

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -218,7 +218,7 @@ async function onConversation() {
     }
 
     const currentChat = getChatByUuidAndIndex(+uuid, dataSources.value.length - 1)
-    const iso1model = currentChatModel.value?.includes('o1')
+    const iso1model = currentChatModel.value?.includes('xxx')
     if (currentChat?.text && currentChat.text !== '' && !iso1model) {
       updateChatSome(
         +uuid,
